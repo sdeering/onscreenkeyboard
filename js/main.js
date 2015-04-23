b87dd1010ab454fbc3ffb55eaee58230607f717d
grburgos <grburgos@gmail.com>
2015-04-23 11:17:54 -0500

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-     $(function () {
-         jsKeyboard.init("virtualKeyboard");
-
-         //first input focus
-         var $firstInput = $(':input').first().focus();
-         jsKeyboard.currentElement = $firstInput;
-         jsKeyboard.currentElementCursorPosition = 0;
-     });
